ffa9da81b687bc130663a46425069c2489c4bb1f
Ivan Vaniuk <ivanvanuk77@gmail.com>
2025-05-11 22:53:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const thumbnail = e.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+
+  largeImg.src = thumbnail.href;
+  largeImg.alt = thumbnail.title;
+});
